9bd93742e9caaa804671f4063caaef4d277ca63e
Reza Shakeri <rzashakeri@gmail.com>
2023-05-01 10:56:34 +0430

    ✅ test: added test_main.py

---
+++ tests/test_main.py
@@ -0,0 +1,22 @@
+import gpt4free
+from gpt4free import Provider, forefront
+
+# usage You
+response = gpt4free.Completion.create(Provider.You, prompt='Write a poem on Lionel Messi')
+print(response)
+
+# usage forefront
+token = forefront.Account.create(logging=False)
+response = gpt4free.Completion.create(
+    Provider.ForeFront, prompt='Write a poem on Lionel Messi', model='gpt-4', token=token
+)
+print(response)
+print(f'END')
+
+# usage theb
+response = gpt4free.Completion.create(Provider.Theb, prompt='Write a poem on Lionel Messi')
+print(response)
+
+# usage cocalc
+response = gpt4free.Completion.create(Provider.CoCalc, prompt='Write a poem on Lionel Messi', cookie_input='')
+print(response)
